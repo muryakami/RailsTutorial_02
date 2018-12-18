3ae3937e6e4f7ccc13c05196c7b5a5e1cdb0b65e
YukiMurakami <y-murakami@nekojarashi.com>
2018-12-19 02:44:21 +0900

    Destroy JournalTasks Controller
    rails destroy controller JournalTasks

--- app/controllers/journal_tasks_controller.rb
+++
@@ -1,2 +0,0 @@
-class JournalTasksController < ApplicationController
-end
--- test/controllers/journal_tasks_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class JournalTasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
